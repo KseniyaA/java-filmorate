2477571ab398cdd84ef93b872f076049328d63c3
Akushkina Kseniya <akushkina.ka@yandex.ru>
2023-03-06 00:05:25 +0300

    sprint 9 - change spring-boot-starter-parent version

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.server.ResponseStatusException;
 
 public class ValidationException extends ResponseStatusException {
